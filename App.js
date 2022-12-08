cf135d86310a8ee3ab4ccffe96136b5c33ce2b8f
NamDo8467 <huynhnamdo@gmail.com>
2022-12-08 01:25:06 -0500

    wrapped the App by Context

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-
+import RestaurantContext from "./components/RestaurantContext"
 import Loading from "./components/Loading"
 import Main from "./components/Main"
 const Stack = createNativeStackNavigator()
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				initialRouteName='Main'
-				screenOptions={{
-					headerShown: false
-				}}
-			>
-				<Stack.Screen name='Loading' component={Loading}></Stack.Screen>
-				<Stack.Screen name='Main' component={Main}></Stack.Screen>
-			</Stack.Navigator>
-		</NavigationContainer>
+		<RestaurantContext>
+			<NavigationContainer>
+				<Stack.Navigator
+					initialRouteName='Main'
+					screenOptions={{
+						headerShown: false
+					}}
+				>
+					<Stack.Screen name='Loading' component={Loading}></Stack.Screen>
+					<Stack.Screen name='Main' component={Main}></Stack.Screen>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</RestaurantContext>
 	)
 }
